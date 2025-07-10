8a7387644551fcd97ce3634b6025c5400b247097
Muhammad-Hamza-Liaqat <mh408800@gmail.com>
2025-07-10 05:38:55 -0700

    update: change GraphQL query from ping to hamza with updated resolver response

--- server/src/config/graphql/graphqlConfig.js
+++ server/src/config/graphql/graphqlConfig.js
@@ -4,13 +4,13 @@ const gql = require('graphql-tag');
 
 const typeDefs = gql`
   type Query {
-    ping: String
+    hamza: String
   }
 `;
 
 const resolvers = {
   Query: {
-    ping: () => 'pong from Apollo Server!',
+    hamza: () => 'Hello, From Hamza GraphQL Server!',
   },
 };
 
